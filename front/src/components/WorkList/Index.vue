6b966a034281f4564e766beee94bb109f36b022a
Dmitry <dmitriy.vlasov@osinit.com>
2019-04-05 00:29:29 +0300

    Верстка элемента списка

--- front/src/components/WorkList/Index.vue
+++ front/src/components/WorkList/Index.vue
@@ -1,18 +1,28 @@
 <template>
   <div>
       <ul class="list-group text-left">
-          <li class="list-group-item">Cras justo odio</li>
-          <li class="list-group-item">Dapibus ac facilisis in</li>
-          <li class="list-group-item">Morbi leo risus</li>
-          <li class="list-group-item">Porta ac consectetur ac</li>
-          <li class="list-group-item">Vestibulum at eros</li>
+          <li class="list-group-item" v-for="item in items" :key="item.id"><WorkListItem :event="item.message"/></li>
       </ul>
   </div>
 </template>
 
 <script>
+import WorkListItem from './WorkListItem'
 export default {
-  name: 'Index'
+  name: 'Index',
+  components: {
+    WorkListItem
+  },
+  data () {
+    return {
+      items: [
+        { message: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.' },
+        { message: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.' },
+        { message: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.' },
+        { message: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.' }
+      ]
+    }
+  }
 }
 </script>
 
---
+++ front/src/components/WorkList/WorkListItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="d-flex justify-content-between align-items-center">
+        <div class="form-check d-flex align-items-center">
+            <input type="checkbox" class="form-check-input mt-0" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">{{event}}</label>
+        </div>
+        <button type="button" class="btn btn-danger ml-1"><i class="fas fa-trash-alt"></i></button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'WorkListItem',
+  props: {event: String}
+}
+</script>
+
+<style scoped>
+
+</style>
