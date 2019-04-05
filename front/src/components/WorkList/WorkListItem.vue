9231ece4bfb366becc87ff26439c182a1dea5afd
Dmitry <dmitriy.vlasov@osinit.com>
2019-04-05 23:43:51 +0300

    Добавлена ячейка с временем

--- front/src/components/WorkList/WorkListItem.vue
+++ front/src/components/WorkList/WorkListItem.vue
@@ -2,7 +2,7 @@
     <div class="d-flex justify-content-between align-items-center">
         <div class="form-check d-flex align-items-center">
             <input type="checkbox" class="form-check-input mt-0" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">{{event}}</label>
+            <label class="form-check-label" for="exampleCheck1"><span class="badge badge-primary">16:00</span> {{event}}</label>
         </div>
         <button type="button" class="btn btn-danger ml-1"><i class="fas fa-trash-alt"></i></button>
     </div>
