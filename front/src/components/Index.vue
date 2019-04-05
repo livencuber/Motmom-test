0f9306621a1d3f88d14d8923fee5844002d5182d
Dmitry <dmitriy.vlasov@osinit.com>
2019-04-05 23:29:30 +0300

    Модальное окно для добавления события

--- front/src/components/Index.vue
+++ front/src/components/Index.vue
@@ -3,9 +3,14 @@
     <div class="jumbotron">
       <div class="d-flex justify-content-between mb-2 align-items-center">
         <h3 class="mb-0">Список событий</h3>
-        <button type="button" class="btn btn-info">Добавить</button>
+        <b-button class="btn btn-info" v-b-modal.AddTask>Добавить</b-button>
       </div>
       <WorkList/>
+    </div>
+    <div>
+      <b-modal centered  ok-only id="AddTask" title="Добавить событие">
+        <b-form-input placeholder="Название"></b-form-input>
+      </b-modal>
     </div>
   </div>
 </template>
