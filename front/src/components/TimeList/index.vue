b0708f24bf81308bf97e7546345360ae17b515de
Dmitry <dmitriy.vlasov@osinit.com>
2019-04-08 02:31:25 +0300

    Блокирование кнопок

--- front/src/components/TimeList/index.vue
+++ front/src/components/TimeList/index.vue
@@ -1,19 +1,34 @@
 <template>
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title text-muted">Время событий</h5>
-            <ul class="list-inline text-left">
-                <li class="list-inline-item mb-1" v-for="item in times" v-bind:key="item.id">
-                    <button type="button" class="btn btn-outline-secondary pt-0 pb-0" @click="setTimes(item)">{{item | moment("HH:mm")}}</button>
-                </li>
-            </ul>
-        </div>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title text-muted">Время событий</h5>
+      <ul class="list-inline text-left">
+        <li
+          class="list-inline-item mb-1"
+          v-for="item in times"
+          v-bind:key="item.id"
+        >
+          <button
+            type="button"
+            class="btn pt-0 pb-0"
+            v-bind:class="[item.disabled ? 'btn-secondary' : 'btn-outline-secondary']"
+            :disabled="!!item.disabled"
+            @click="setTimes(item.time)"
+          >
+            {{ item.time | moment("utc", "HH:mm") }}
+          </button>
+        </li>
+      </ul>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'index',
+  created () {
+    this.$store.commit('setTimes')
+  },
   computed: {
     times () {
       return this.$store.getters.times
@@ -21,12 +36,10 @@ export default {
   },
   methods: {
     setTimes (item) {
-      this.$store.dispatch('changeTime', item.utc().toString())
+      this.$store.dispatch('changeTime', item)
     }
   }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
