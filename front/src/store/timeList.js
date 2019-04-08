503fc16361cba3d5d7a98f655662a0a1b06fb254
Dmitry <dmitriy.vlasov@osinit.com>
2019-04-08 20:40:18 +0300

    Время из state

--- front/src/store/timeList.js
+++ front/src/store/timeList.js
@@ -9,57 +9,70 @@ Vue.use(Notifications)
 
 const moment = extendMoment(Moment)
 
-const timeList =
-  {
-    state: {
-      time_start: '18:00',
-      time_end: '24:00',
-      time_interval: 30,
-      times: []
-    },
-    actions: {
-      changeTime ({dispatch, commit}, time) {
-        const data = {
-          time: moment.unix(time).utc().format(),
-          tasks: this.getters.checkedTask.map((item) => (item.id))
-        }
-        api.changeTime(data).then((responce) => {
-          Vue.notify({
-            title: 'Время изменено',
-            type: 'success'
-          })
-          dispatch('fetchTasks')
-          commit('enableAllTime')
-        })
+const timeList = {
+  state: {
+    time_start: '15:00',
+    time_end: '24:00',
+    time_interval: 45,
+    times: []
+  },
+  actions: {
+    changeTime ({ dispatch, commit }, time) {
+      const data = {
+        time: moment
+          .unix(time)
+          .utc()
+          .format(),
+        tasks: this.getters.checkedTask.map(item => item.id)
       }
-
-    },
-    getters: {
-      times: state => {
-        return state.times
-      }
-    },
-    mutations: {
-      setTimes (state) {
-        const range = moment.range(moment.utc(0).set({h: 18}).format(), moment.utc(0).set({h: 24}).format())
-        const times = Array.from(range.by('minutes', {step: state.time_interval}))
-        state.times = times.map((item) => {
-          return { time: item.unix(), disabled: false }
+      api.changeTime(data).then(responce => {
+        Vue.notify({
+          title: 'Время изменено',
+          type: 'success'
         })
-      },
-      disabledTime (state, data) {
-        const time = _.find(state.times, {time: moment(data.time).unix()})
-        if (time) {
-          time.disabled = true
-        }
-      },
-      enableAllTime (state) {
-        state.times = state.times.map((item) => {
-          return {...item, disabled: false}
-        })
-      }
+        dispatch('fetchTasks')
+        commit('enableAllTime')
+      })
     }
-
+  },
+  getters: {
+    times: state => {
+      return state.times
+    }
+  },
+  mutations: {
+    setTimes (state) {
+      const timeStartArr = state.time_start.split(':')
+      const timeEndArr = state.time_end.split(':')
+      const range = moment.range(
+        moment
+          .utc(0)
+          .set({ h: timeStartArr[0], m: timeStartArr[1] })
+          .format(),
+        moment
+          .utc(0)
+          .set({ h: timeEndArr[0], m: timeEndArr[1] })
+          .format()
+      )
+      const times = Array.from(
+        range.by('minutes', { step: state.time_interval })
+      )
+      state.times = times.map(item => {
+        return { time: item.unix(), disabled: false }
+      })
+    },
+    disabledTime (state, data) {
+      const time = _.find(state.times, { time: moment(data.time).unix() })
+      if (time) {
+        time.disabled = true
+      }
+    },
+    enableAllTime (state) {
+      state.times = state.times.map(item => {
+        return { ...item, disabled: false }
+      })
+    }
   }
+}
 
 export default timeList
